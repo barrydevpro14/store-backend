2a57513b482dec8afe58f6f1d9e2f1aa8ec7fe93
barrydevpro14 <barrydevpro@gmail.com>
2026-09-12 13:35:45 +0000

    update unite mesure

--- src/main/java/org/store/produit/domain/enums/UniteMesureEnum.java
+++ src/main/java/org/store/produit/domain/enums/UniteMesureEnum.java
@@ -1,7 +1,8 @@
 package org.store.produit.domain.enums;
 
 public enum UniteMesureEnum {
-    PIECE, SAC, KG, LITRE, METRE, METRE_CARRE, CARTON;
+    PIECE, SAC, KG, LITRE, METRE, METRE_CARRE, CARTON,
+    METRE_CUBE, SACHET, CENTIMETRE, MILLIMETRE, GRAMME;
 
     public String code() {
         return name();
